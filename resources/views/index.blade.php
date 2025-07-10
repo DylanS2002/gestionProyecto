8e7a21ab78cd399baabf728376264a5784777092
DylanS <saavedra.dylan17@gmail.com>
2025-07-09 22:55:52 -0500

    se creo y se enlazo el archivo para los estilos a index general

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.app2')
 @extends('adminlte::page')
 
 @section('title', 'ELITEfox-Inicio')
@@ -95,94 +96,6 @@
 @stop
 
 @section('css')
-    <style>
-    
-</style>
-    <style>
-        .main-sidebar {
-            background-image: url("https://i.pinimg.com/736x/19/d9/b5/19d9b576468ba999e2c49582a5879599.jpg");
-            background-size: cover; 
-            background-repeat: no-repeat; 
-            background-position: center center;
-}
-    </style>
-
-    <style>
-        .content-wrapper {
-            background-image: url("https://i.pinimg.com/736x/19/d9/b5/19d9b576468ba999e2c49582a5879599.jpg");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-            background-attachment: fixed;
-            color: #c19d44;
-        }
-    </style>
-    <style>
-        .main-header {
-            background-image: url("https://media.istockphoto.com/id/1471197299/es/foto/textura-de-pared-de-lujo-de-mármol-negro-y-dorado-con-patrón-de-línea-dorada-brillante-diseño.jpg?s=612x612&w=0&k=20&c=UzevK4naddrICZDJYcgn6YHckyCRfkenizZdDgurup8=");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-            color: black !important;
-        }
-
-        .main-header .nav-link, 
-        .main-header .navbar-nav .nav-item > a {
-            color: #c19d44 !important;
-        }
-    </style>
-
-
-    
-    <style>
-        .nav-sidebar .nav-link {
-            transition: color 0.3s, text-shadow 0.3s;
-        }
-
-        .nav-sidebar .nav-link:hover {
-            color: #fff;
-            text-shadow: 0 0 8px #c19d44, 0 0 12px #c19d44; /* color neón azul */
-        }
-    </style>
-    <style>
-        .nav-sidebar .nav-link {
-            transition: color 0.3s ease-in-out;
-        }
-
-        .nav-sidebar .nav-link:hover {
-            color: #c19d44; /* texto se ilumina con otro color */
-        }
-    </style>
-
-    <style>
-        .nav-sidebar .nav-link:hover {
-            color: #fff;
-            text-shadow:
-                0 0 5px #c19d44,
-                0 0 10px #c19d44,
-                0 0 20px #c19d44,
-                0 0 40px #c19d44;
-        }
-    </style>
-
-    <style>
-        .nav-sidebar .nav-link {
-            color: #ccc; /* color base */
-            transition: color 0.3s ease;
-        }
-
-        .nav-sidebar .nav-link:hover {
-            color: #c19d44 !important; /* color al pasar el mouse */
-        }
-    </style>
-
-    <style>
-
-        .nav-sidebar .nav-link.active {
-            background-color: #c19d44; /* Color cuando está activo */
-            color: #fff;
-        }
-    </style>
 
 @stop
 
---
+++ resources/views/layouts/app2.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <style>
+        .main-sidebar {
+            background-image: url("https://i.pinimg.com/736x/19/d9/b5/19d9b576468ba999e2c49582a5879599.jpg");
+            background-size: cover; 
+            background-repeat: no-repeat; 
+            background-position: center;
+}
+    </style>
+
+    <style>
+        .content-wrapper {
+            background-image: url("https://i.pinimg.com/736x/19/d9/b5/19d9b576468ba999e2c49582a5879599.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+            color: #c19d44;
+        }
+    </style>
+    <style>
+        .main-header {
+            background-image: url("https://media.istockphoto.com/id/1471197299/es/foto/textura-de-pared-de-lujo-de-mármol-negro-y-dorado-con-patrón-de-línea-dorada-brillante-diseño.jpg?s=612x612&w=0&k=20&c=UzevK4naddrICZDJYcgn6YHckyCRfkenizZdDgurup8=");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            color: black !important;
+        }
+
+        .main-header .nav-link, 
+        .main-header .navbar-nav .nav-item > a {
+            color: #c19d44 !important;
+        }
+    </style>
+
+
+    
+    <style>
+        .nav-sidebar .nav-link {
+            transition: color 0.3s, text-shadow 0.3s;
+        }
+
+        .nav-sidebar .nav-link:hover {
+            color: #fff;
+            text-shadow: 0 0 8px #c19d44, 0 0 12px #c19d44; /* color neón azul */
+        }
+    </style>
+    <style>
+        .nav-sidebar .nav-link {
+            transition: color 0.3s ease-in-out;
+        }
+
+        .nav-sidebar .nav-link:hover {
+            color: #c19d44; /* texto se ilumina con otro color */
+        }
+    </style>
+
+    <style>
+        .nav-sidebar .nav-link:hover {
+            color: #fff;
+            text-shadow:
+                0 0 5px #c19d44,
+                0 0 10px #c19d44,
+                0 0 20px #c19d44,
+                0 0 40px #c19d44;
+        }
+    </style>
+
+    <style>
+        .nav-sidebar .nav-link {
+            color: #ccc; /* color base */
+            transition: color 0.3s ease;
+        }
+
+        .nav-sidebar .nav-link:hover {
+            color: #c19d44 !important; /* color al pasar el mouse */
+        }
+    </style>
+
+    <style>
+
+        .nav-sidebar .nav-link.active {
+            background-color: #c19d44; /* Color cuando está activo */
+            color: #fff;
+        }
+    </style>
+
+</body>
+</html>
